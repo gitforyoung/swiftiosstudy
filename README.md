524c74a9f4f9d4bfd573852ec2e47d4c936017ae
gitforyoung <61397165+gitforyoung@users.noreply.github.com>
2022-11-18 17:40:02 +0900

    combine study

--- README.md
+++ README.md
@@ -4,12 +4,14 @@
 - (강의) [Sololearn - Swift 4](https://www.sololearn.com/learning/1075)
 - (블로그) [Pingu님 블로그 - class와 structure의 차이](https://icksw.tistory.com/271)
 - (블로그) [김준오님 블로그 - ARC란](https://velog.io/@kjo1130/ARC%EB%9E%80)
+- (블로그) [Hyunddy님 블로그 - Retain Cycle, weak self](https://hyunndyblog.tistory.com/154)
 - (블로그) [소들님 블로그 - 메모리 구조](https://babbab2.tistory.com/25)
 - (블로그) [Pingu님 블로그 - Combine](https://icksw.tistory.com/271)
 - (블로그) [해리님 블로그 - Property Wrapper](https://medium.com/harrythegreat/swift-properywrapper를-이용한-값처리-a8ef0d87e8e)
 - (블로그) [Weekoding님 블로그 - DispatchQueue와 스레드 개념](https://weekoding.tistory.com/8)
 - (블로그) [Coden님 블로그 - DispatchQueue.main vs RunLoop.main](https://velog.io/@ictechgy/Combine-DispatchQueue.main-VS-RunLoop.main)
-
+- (블로그) [HoonIOS님 블로그 - UISearchController](https://boidevelop.tistory.com/110)
+- (블로그) [김영채님 블로그 - Navigation Item](https://velog.io/@kevinkim2586/iOS-Swift-공부-Navigation-Item)
 ## CS 학습 목록
 - (도서) [혼자 공부하는 컴퓨터구조 + 운영체제](http://www.yes24.com/Product/Goods/111378840)
 - (도서) [쓰면서 익히는 알고리즘과 자료구조](http://www.yes24.com/Product/Goods/98828718)
--- doit-swiftios/Combine_debounce/Combine_debounce/ViewController.swift
+++ doit-swiftios/Combine_debounce/Combine_debounce/ViewController.swift
@@ -6,14 +6,56 @@
 //
 
 import UIKit
+import Combine
 
 class ViewController: UIViewController {
 
+    @IBOutlet var lblText: UILabel!
+    
+    // UISearchController(view controller): search bar와 상호작용하는 것을 바탕으로 검색 결과를 보여주는 것을 관리하는 뷰컨트롤러
+    private lazy var searchController: UISearchController = {
+        let searchController = UISearchController()
+        // obscureBackgroundDuringPresentation는 search bar로 최초 응답자가 가게 되면 나머지 뷰들을 어둡게 할 것인지 설정
+        searchController.obscuresBackgroundDuringPresentation = false
+        searchController.searchBar.tintColor = .systemPink
+        searchController.searchBar.searchTextField.accessibilityIdentifier = "mySearchBarTextField"
+        return searchController
+    }()
+    
+    var mySubscription = Set<AnyCancellable>()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // Navigation Controller를 임베디드 했으므로 navigationItem이  있음. searchController를 위해서 정의한 searchController로 설정함. searchController를 lazy로 설정했기 때문에 이때 호출될 때 메모리로 올라감.
+        self.navigationItem.searchController = searchController
+        searchController.isActive = true
+        
+        searchController.searchBar.searchTextField
+            .myDebounceSearchPublisher
+        // 클로저 안에서 self를 쓸 때 순환참조 문제 발생. [weak self] 사용하여 약한 참조.
+            .sink { [weak self] (receivedValue) in
+                print("receivedValue : \(receivedValue)")
+                self?.lblText.text = receivedValue
+            }
+            .store(in: &mySubscription)
     }
 
 
 }
 
+extension UISearchTextField {
+    var myDebounceSearchPublisher: AnyPublisher<String, Never> {
+        //.publishcer(for:object:) - for:publish할 notification, object:notification을 posting하는 object
+        NotificationCenter.default.publisher(for: UITextField.textDidChangeNotification, object: self)
+            .compactMap{ $0.object as? UISearchTextField }
+            .map{ $0.text ?? ""}
+        // debounce : Publishes elements only after a specified time interval elapses between events.
+        // debounce가 없는 경우 타이핑할 때마다 값을 받아 동작하기 때문에 리소스 낭비가 심해짐.
+            .debounce(for: .milliseconds(1000), scheduler: RunLoop.main)
+        // filter를 이용해서 글자가 있을 때만 값을 전달하도록 함.
+            .filter{ $0.count > 0 }
+            .print()
+            .eraseToAnyPublisher()
+    }
+}
